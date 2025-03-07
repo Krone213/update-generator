df6909f03e65fcd32eb8bdcbe0291a9a5312ec2c
Roman <rrromanow@bk.ru>
2025-03-07 09:19:19 +0300

    Сука

---
+++ mainwindow.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_centralwidget_destroyed()
+{
+
+}
+
+
+void MainWindow::on_StartFirmwareMenu_customContextMenuRequested(const QPoint &pos)
+{
+
+}
+
