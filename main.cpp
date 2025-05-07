8f3744a47725ce1a8acf5172c040d50b5c8e0b21
Krone213 <50117088+Krone213@users.noreply.github.com>
2025-05-07 23:47:29 +0300

    Настройка масштабирования

--- main.cpp
+++ main.cpp
@@ -2,9 +2,14 @@
 
 #include <QApplication>
 #include <QStyleFactory>
+#include <QGuiApplication> 
+#include <QCoreApplication> 
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
     QApplication a(argc, argv);
 
     a.setStyle(QStyleFactory::create("Fusion"));
